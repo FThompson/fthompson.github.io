5b385d73dd4d6d1954228a209c7b78416280e5c6
Finn Thompson <nnifinn@gmail.com>
2019-04-04 15:54:50 -0700

    Added View Report option to replace some Read More project pages.

--- static/scripts/portfolio.js
+++ static/scripts/portfolio.js
@@ -13,10 +13,15 @@ function addProject(project) {
         $('<span>').addClass('tag').text(tag).appendTo(tagsDiv)
     })
     $('<p>').html(project.summary).appendTo(card)
+    if (project.report) {
+        let href = '/static/projects/reports/' + project.report
+        $('<a>').attr('href', href).attr('title', 'View report').attr('target', '_blank')
+                .text('View report').appendTo(card)
+    }
     if (project.content) {
         // TODO: convert site to jekyll or other solution to avoid hashbang usage
         let hash = '#!/' + project.content.slice(0, project.content.length - 3)
-        $('<a>').attr('href', hash).attr('title', 'Expand').text('Read more').appendTo(card)
+        $('<a>').attr('href', hash).attr('title', 'Read more').text('Read more').appendTo(card)
     }
     let bottom = $('<div>').addClass('bottom').appendTo(card)
     $('<span>').text(project.date).appendTo(bottom)
